9f6a7db60594bd49a17b3417df9dc21aba324bfd
Darren Murphy <darren@victoriajd.com>
2021-10-02 17:21:12 +0800

    Drop seconds

--- function.js
+++ function.js
@@ -15,7 +15,7 @@ window.function = function (seconds) {
   if (days > 0) segments.push(days + ' day' + ((days == 1) ? '' : 's'));
   if (hours > 0) segments.push(hours + ' hour' + ((hours == 1) ? '' : 's'));
   if (minutes > 0) segments.push(minutes + ' minute' + ((minutes == 1) ? '' : 's'));
-  if (seconds > 0) segments.push(seconds + ' second' + ((seconds == 1) ? '' : 's'));
+  //if (seconds > 0) segments.push(seconds + ' second' + ((seconds == 1) ? '' : 's'));
   var uptime_string = segments.join(', ');
   return uptime_string;
 }
